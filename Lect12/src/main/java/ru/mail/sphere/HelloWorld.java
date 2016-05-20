38120e816ce23f014a58036ccac016584479e347
Mikhail Postnikov <mihanlg@yandex.ru>
2016-05-20 18:24:36 +0300

    Java

---
+++ Lect12/src/main/java/ru/mail/sphere/HelloWorld.java
@@ -0,0 +1,15 @@
+package ru.mail.sphere;
+
+
+public class HelloWorld {
+
+    public static void main(String... args) {
+        System.out.println("Hello world!");
+    }
+
+
+
+    public String getHelloWorld() {
+        return "HelloWorld";
+    }
+}
